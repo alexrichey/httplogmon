d26787cbab63df4b98fe7910779d5708b7aa874c
Alexander Richey <alex.richey@bluewolf.com>
2019-02-18 14:56:58 -0500

    Document methods, move methods around, redo log cleaning method

--- src/log_mon.js
+++ src/log_mon.js
@@ -5,10 +5,13 @@ var follow = require('text-file-follower'),
     logParser = require('clf-parser');
 
 
+/**
+ * Monitor for the specified log file. The file will be tailed and new log lines will be parsed
+ * and cached. Old logs are removed from the cache asynchronously at a given interval.
+ * Additionally, when logs are parsed, stats will be generated for certain topics like api sections hits.
+ */
 var LogMonitor = function(config) {
-  this.DISREGARD_LOG_TIMESTAMP = config.ignoreOldTimestampLogs || false;
-  this.RECOVERY_TYPE = 'recover';
-  this.BREACH_TYPE = 'breach';
+  this.DISREGARD_LOG_TIMESTAMP = config.ignoreOldTimestampLogs;
   this.ALARM_LOG_COUNT_THRESHOLD = config.alarmLogCountThreshold || 10;
   this.CACHED_LOG_RETENTION_SECONDS = config.logCacheRetentionTimeSeconds || 2 * 60;
 
@@ -19,12 +22,16 @@ var LogMonitor = function(config) {
     fs.writeFileSync(this.logFilePath, null);
   }
 
-  this.shortTermHitCount = 0;
+  this.RECOVERY_TYPE = 'recover';
+  this.BREACH_TYPE = 'breach';
+
+  // long term stats
   this.logs = [];
   this.trafficAlerts = [];
   this.errors = [];
 
   // cached stats
+  this.shortTermHitCount = 0;
   this.cachedApiSectionHits = {};
   this.cachedUserHits = {};
 
@@ -43,6 +50,11 @@ var LogMonitor = function(config) {
     }
   };
 
+  /**
+   * Returns the section (uri portion before the second slash) from a given uri
+   * @param  {String} uri
+   * @return {String} section
+   */
   this.getSectionFromUri = (uri) => {
     // set i is 1 to skip the first occurence of '/'
     for (var i = 1; i < uri.length; i++) {
@@ -82,6 +94,16 @@ var LogMonitor = function(config) {
     return _.take(this.makeSortedHitCountPairs(this.cachedApiSectionHits), n);
   };
 
+  this.makeLastNRequestsList = (n) => {
+    if (this.logs.length === 0) {
+      return [{'remote_addr': '', 'remote_user': '', 'time_local': '', 'request': ''}];
+    } else {
+      return _.takeRight(this.logs, n).map((line) => {
+        return _.pick(line, 'remote_addr', 'remote_user', 'time_local', 'request');
+      });
+    }
+  };
+
   this.clearCachedStats = () => {
     this.shortTermHitCount = 0;
     this.cachedApiSectionHits = {};
@@ -89,7 +111,6 @@ var LogMonitor = function(config) {
   };
 
   this.soundTheAlarm = () => {
-    var self = this;
     this.trafficAlerts.push({type: this.BREACH_TYPE, alert_time: (new Date).toLocaleString(), hits: this.logs.length});
   };
 
@@ -102,10 +123,13 @@ var LogMonitor = function(config) {
     return previousAlert && previousAlert.type === this.BREACH_TYPE;
   };
 
-  this.printTrafficAlerts = (logMonitor) => {
+  /**
+   * Print Traffic Alerts with the most recent first
+   */
+  this.printTrafficAlerts = () => {
     var trafficAlertsCopy = this.trafficAlerts.slice(0);
     var highTrafficAlerts = _.reverse(trafficAlertsCopy);
-    return highTrafficAlerts.map(r => {
+    return highTrafficAlerts.forEach(r => {
       if (r.type === this.BREACH_TYPE) {
         console.log(chalk.red(`High traffic generated an alert - hits = ${r.hits}, triggered at ${r.alert_time}`));
       } else {
@@ -129,27 +153,35 @@ var LogMonitor = function(config) {
     this.generateAlarmsOrRecovery();
   };
 
+  /**
+   * Removes logs that are past the long term retention datetime.
+   * If DISREGARD_LOG_TIMESTAMP is true, use the time that the log as processed for comparison
+   */
   this.removeOldLogs = () => {
     var limit = new Date();
     limit.setSeconds(limit.getSeconds() - this.CACHED_LOG_RETENTION_SECONDS);
 
-    for (var i = this.logs.length - 1; i >= 0; i--) {
-      var log = this.logs[i];
-      var logDate = this.DISREGARD_LOG_TIMESTAMP ? log.processed_at : log.time_local;
-
-      if (new Date(logDate) < limit) {
-        this.logs.splice(i, 1);
-      }
-    }
+    var self = this;
+    _.remove(this.logs, (log) => {
+      var logDate = self.DISREGARD_LOG_TIMESTAMP ? log.processed_at : log.time_local;
+      return (new Date(logDate) < limit);
+    })
   };
 
+
   this.topTrafficBreach = -1;
+  /**
+   * Generates traffic alerts based on whether the threshold has been breached.
+   * Note: since logs are processed async, the breach may be registered before all the logs
+   * are processed, which might be misleading when reporting the hits. To Rememdy this,
+   * while the breach is active, we update the actual max number of logs and reset upon recovery
+   */
   this.generateAlarmsOrRecovery = () => {
     var trafficAlertActive = this.trafficAlertActive();
     if (trafficAlertActive) {
       this.topTrafficBreach = Math.max(this.topTrafficBreach, this.logs.length);
       if (this.logs.length < this.ALARM_LOG_COUNT_THRESHOLD) {
-        // Set the actual breach number on the latest traffic alert
+        // Set the actual breach hits number on the latest traffic alert
         var latestAlert = this.trafficAlerts[this.trafficAlerts.length - 1];
         latestAlert.hits = this.topTrafficBreach;
         this.topTrafficBreach = -1;
@@ -160,16 +192,10 @@ var LogMonitor = function(config) {
     }
   };
 
-  this.makeLastNRequestsList = (n) => {
-    if (this.logs.length === 0) {
-      return [{'remote_addr': '', 'remote_user': '', 'time_local': '', 'request': ''}];
-    } else {
-      return _.takeRight(this.logs, n).map((line) => {
-        return _.pick(line, 'remote_addr', 'remote_user', 'time_local', 'request');
-      });
-    }
-  };
-
+  /**
+   * Starts the Log Monitor. While active, it will tail the specified log file, and process the results asynchronously.
+   * @return {[type]} [description]
+   */
   this.start = () => {
     self = this;
     self.asyncProcessLogsLoop();
